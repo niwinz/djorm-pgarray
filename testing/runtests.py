7ef436dc909fdcb3ba917faddda585f8619bc5ed
Carl Meyer <carl@oddbird.net>
2014-05-30 13:02:58 -0600

    Fix running the tests against Django 1.7.

--- testing/runtests.py
+++ testing/runtests.py
@@ -6,6 +6,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 from django.core.management import call_command
 
 if __name__ == "__main__":
+    import django
+    if django.VERSION[:2] >= (1, 7):
+        django.setup()
     args = sys.argv[1:]
     if len(args) == 0:
         args.append("pg_array_fields")
